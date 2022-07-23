903848e75831d70c3d0901277e7da8e3c0310e4b
Faraz Ahmed Shaikh <lets_play879@yahoo.com>
2022-07-23 07:42:13 -0700

    fixes

--- reactjs/src/components/TeamViewBox/index.js
+++ reactjs/src/components/TeamViewBox/index.js
@@ -16,7 +16,7 @@ const TeamViewBox = ({ data = [] }) => {
               <Link to={'/teamProfile/' + e.id}>
                 <Card.Grid key={index2} style={{width: '20%'}}>
                   <Tooltip title={e.name}>
-                    <Card key={index2} cover={<img alt="example" src={getImage(e.url)} />} bodyStyle={{ padding: 0 }}>
+                    <Card key={index2} cover={<img alt="example" src={getImage(e.profileUrl)} />} bodyStyle={{ padding: 0 }}>
                       <div style={{ width: '100%', border: '1px solid lightgray', padding: '10px' }}>{truncateText(e.name, 20)}</div>
                     </Card>
                   </Tooltip>
